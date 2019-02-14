27c82b95de952cc5cf4eb64c86d9cde4ee2da3c4
s69 <47628442+s69@users.noreply.github.com>
2019-02-14 18:03:43 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# s69.github.io
+# S69
+Link -> <a href="https://s69.github.io/">Click Here To Enter</a>
+***
+> *The unexamined life is not worth living.*
+> 　　　　　　　　　　　　　*--Socrates*
+***
---
+++ js/main.js
@@ -0,0 +1,43 @@
+document.onkeydown = function (e) {
+    var currKey = 0,
+        evt = e || window.event;
+    currKey = evt.keyCode || evt.which || evt.charCode;
+    if (currKey == 123) {
+        window.event.cancelBubble = true;
+        window.event.returnValue = false;
+    }
+}
+
+document.onmousedown = function (event) {
+    if (event.which == 1) {
+        return false;
+    } else if (event.which == 2) {
+        return false;
+    }
+}
+
+document.oncontextmenu = function () {
+    return false;
+}
+
+window.onload = int = setInterval(function () {
+    var divInput = document.getElementById("divInput");
+    var divInputTwo = document.getElementById("divInputTwo");
+    var divPasswordTwo = document.getElementById("divPasswordTwo");
+    var divOffP = document.getElementsByTagName("p");
+    var divOff = document.getElementById("divOff");
+    divInput.focus();
+    if (divInput.value == "bing") {
+        divInput.style.display = "none";
+        divOffP[0].style.display="none";
+        divPasswordTwo.style.display = "block";
+        divInputTwo.focus();
+        if (divInputTwo.value == "googlebing") {
+            divInputTwo.style.display = "none";
+            divOffP[1].style.display="none";
+            divOff.style.display = "block";
+            document.getElementsByTagName("body")[0].style.backgroundColor = "#fff";
+            clearInterval(int);
+        }
+    }
+}, 2000);
